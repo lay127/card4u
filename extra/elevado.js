0ef85fe5999ace6332c30dee9f87674639f00df6
Layla <160488656+lay127@users.noreply.github.com>
2024-08-05 18:51:05 -0300

    Delete extra directory

--- extra/elevado.js
+++
@@ -1,9 +0,0 @@
-var n = parseFloat(prompt("Escreva n: "));
-var n1 = parseFloat(prompt("Escreva n1: "));
-var elevado;
-
-elevado = Math.pow(n, n1);
-
-window.alert("UUU seu elevado é: " + elevado);
-
-
