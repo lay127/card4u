d3f45bf1f5c804ed26789622c2d75347834371fa
Layla <160488656+lay127@users.noreply.github.com>
2024-08-07 19:08:17 -0300

    Delete card4u directory

--- card4u/showSuccess.test.js
+++
@@ -1,5 +0,0 @@
-const {showSuccess} = require('./script')
-
-it('mostrar se o envio deu certo',()=>{
-    expect(showSuccess("mensage")).toBe(true)
-})
--- card4u/validateAge.test.js
+++
@@ -1,5 +0,0 @@
-const {validateAge} = require('./script')
-
-it('validar a idade',()=>{
-    expect(validateAge('19')).toBe(true)
-})
